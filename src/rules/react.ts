c4da6c575c461e7ebfb5c5c151e5f33fa79a26fa
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2024-11-02 16:29:04 -0500

    refactor: ♻️ favor `react` recommended rules (#42)

--- src/rules/react.ts
+++ src/rules/react.ts
@@ -1,17 +1,19 @@
 import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactPlugin from "eslint-plugin-react";
 
 import type { Rules } from "../types";
 
+import { normalizeRuleEntries } from "../utils/normalize-rule-entries";
+
 export const reactRules = {
   ...jsxA11y.configs.recommended.rules,
-
+  ...normalizeRuleEntries(reactPlugin.configs.flat?.recommended?.rules),
+  ...normalizeRuleEntries(reactPlugin.configs.flat?.["jsx-runtime"]?.rules),
   "react/boolean-prop-naming": "off", // revisit
   "react/button-has-type": "error",
-
   "react/checked-requires-onchange-or-readonly": "error",
   "react/default-props-match-prop-types": "error",
   "react/destructuring-assignment": "off", // revisit
-  "react/display-name": "error",
   "react/forbid-component-props": "off",
   "react/forbid-dom-props": "off",
   "react/forbid-elements": "off",
@@ -25,54 +27,33 @@ export const reactRules = {
   "react/jsx-filename-extension": "off",
   "react/jsx-fragments": ["error", "syntax"],
   "react/jsx-handler-names": "off",
-  "react/jsx-key": "error",
   "react/jsx-max-depth": "off",
   "react/jsx-no-bind": "off", // revisit
-  "react/jsx-no-comment-textnodes": "error",
   "react/jsx-no-constructed-context-values": "error",
-
-  "react/jsx-no-duplicate-props": "error",
-
   "react/jsx-no-leaked-render": "error",
   "react/jsx-no-literals": "off",
-
   "react/jsx-no-script-url": "error",
-  "react/jsx-no-target-blank": "error",
-  "react/jsx-no-undef": "error",
   "react/jsx-no-useless-fragment": "error",
   "react/jsx-one-expression-per-line": "off",
   "react/jsx-pascal-case": ["error", { allowNamespace: true }],
   "react/jsx-props-no-spreading": "off",
   "react/jsx-sort-default-props": "off",
   "react/jsx-sort-props": "off",
-  "react/jsx-uses-react": "off",
-  "react/jsx-uses-vars": "error",
   "react/no-access-state-in-setstate": "error",
   "react/no-adjacent-inline-elements": "off",
   "react/no-array-index-key": "off",
   "react/no-arrow-function-lifecycle": "error",
-  "react/no-children-prop": "error",
   "react/no-danger": "off",
-  "react/no-danger-with-children": "error",
-  "react/no-deprecated": "error",
   "react/no-did-mount-set-state": "error",
   "react/no-did-update-set-state": "error",
-  "react/no-direct-mutation-state": "error",
-  "react/no-find-dom-node": "error",
   "react/no-invalid-html-attribute": "error",
-  "react/no-is-mounted": "error",
   "react/no-multi-comp": "off",
   "react/no-namespace": "error",
   "react/no-object-type-as-default-prop": "off", // revisit
   "react/no-redundant-should-component-update": "error",
-  "react/no-render-return-value": "error",
   "react/no-set-state": "off",
-  "react/no-string-refs": "error",
   "react/no-this-in-sfc": "error",
   "react/no-typos": "error",
-  "react/no-unescaped-entities": "error",
-  "react/no-unknown-property": "error",
-  "react/no-unsafe": "off",
   "react/no-unstable-nested-components": "error",
   "react/no-unused-class-component-methods": "error",
   "react/no-unused-prop-types": "error",
@@ -82,11 +63,8 @@ export const reactRules = {
   "react/prefer-exact-props": "off",
   "react/prefer-read-only-props": "off",
   "react/prefer-stateless-function": "off",
-  "react/prop-types": "error",
-  "react/react-in-jsx-scope": "off",
   "react/require-default-props": "off",
   "react/require-optimization": "off",
-  "react/require-render-return": "error",
   "react/self-closing-comp": "error",
   "react/sort-comp": "off",
   "react/sort-default-props": "off",
