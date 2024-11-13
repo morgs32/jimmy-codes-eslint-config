264fefb40143e1ade44d21b36f98d032255dcacb
jimmy-guzman <hi@jimmyguzman.com>
2024-11-12 18:41:48 -0600

    feat: ✨ leverage `typescript-eslint`'s new `projectService`
    🏁 Closes: #59

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,7 @@ export type Rules = RuleOptions;
 export interface TypescriptOptions {
   /**
    * Location of `tsconfig.json` used for [type aware linting](https://typescript-eslint.io/getting-started/typed-linting)
-   * @default "./tsconfig.json"
+   * @deprecated since this config uses `projectService` this is no longer needed and will be removed.
    */
   project: string | string[];
 }
