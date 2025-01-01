8db58127659c1d0371037d6dc47f119b7ba2c4c3
jimmy-guzman <hi@jimmyguzman.com>
2025-01-01 14:31:51 -0600

    feat: ✨ bump related `react` eslint deps to latest

--- src/rules/node.ts
+++ src/rules/node.ts
@@ -1,6 +1,6 @@
 import type { Rules } from "../types";
 
 export const nodeRules = {
-  "n/no-process-exit": "off", // TODO [2024-12-31]: enable this rule
+  "n/no-process-exit": "off", // TODO [2025-12-31]: enable this rule
   "n/prefer-node-protocol": "error",
 } satisfies Rules;
