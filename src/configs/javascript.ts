c687aef152cd03f6b466fa6827ce2af9eb598013
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-03-11 19:28:53 -0500

    fix: 🐛 disable `no-magic-numbers` in tests (#145)

--- src/configs/javascript.ts
+++ src/configs/javascript.ts
@@ -1,5 +1,6 @@
 import type { TypedConfigItem } from "../types";
 
+import { GLOB_TESTS } from "../constants";
 import { javascriptRules } from "../rules/javascript";
 
 export const javascriptConfig = () => {
@@ -10,6 +11,13 @@ export const javascriptConfig = () => {
       },
       name: "jimmy.codes/javascript",
       rules: javascriptRules,
+    },
+    {
+      files: GLOB_TESTS,
+      name: "jimmy.codes/javascript/testing",
+      rules: {
+        "no-magic-numbers": "off",
+      },
     },
   ] satisfies TypedConfigItem[];
 };
