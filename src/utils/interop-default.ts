f98c484cca3ace9a622e02dcf14db56919704bea
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-03-11 20:46:03 -0500

    fix: 🐛 require one blank line before first jsdoc tag (#147)

--- src/utils/interop-default.ts
+++ src/utils/interop-default.ts
@@ -3,6 +3,7 @@
  * Utility to safely fetch an imported `ESLint` plugin.
  *
  * Since ESLint Flat Config still isn't widely adopted, many plugins don't include types or a default import by default. Additionally, since we accept large version ranges in our peer dependencies, the structure of plugins may change at times. This function normalizes an import of a generic ESLint plugin, ensuring it is typed and accessible.
+ *
  * @param module the result of `import('eslint-plugin-name')`.
  *
  * @returns The normalized and awaited default export.
