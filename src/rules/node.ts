bf61352de78fce65353b805e5e2789544d1a83cb
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-03-18 19:24:16 -0500

    feat: ✨ enable a few `node` rules (#152)
    🏁 Closes: #55

--- src/rules/node.ts
+++ src/rules/node.ts
@@ -1,6 +1,13 @@
 import type { Rules } from "../types";
 
 export const nodeRules = {
+  "n/handle-callback-err": ["error", "^(err|error)$"],
+  "n/no-deprecated-api": "error",
+  "n/no-exports-assign": "error",
+  "n/no-new-require": "error",
+  "n/no-path-concat": "error",
   "n/no-process-exit": "off", // TODO [2025-12-31]: enable this rule
+  "n/prefer-global/console": ["error", "always"],
   "n/prefer-node-protocol": "error",
+  "n/process-exit-as-throw": "error",
 } satisfies Rules;
